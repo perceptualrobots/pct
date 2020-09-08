a40bf8ffb826aea30828a03b6280358dc0eaf29f
Rupert Young <rupertjyoung@gmail.com>
2020-09-08 19:17:01 +0100

    update

--- pct/hierarchy.py
+++ pct/hierarchy.py
@@ -155,12 +155,13 @@ class PCTHierarchy():
 
 
     def draw(self, with_labels=True,  font_size=12, font_weight='bold', node_color='red',
-             node_size=500, arrowsize=25, align='horizontal', file=None):
+             node_size=500, arrowsize=25, align='horizontal', file=None, figsize=(8,8)):
         graph = self.graph()
-        #pos=graphviz_layout(graph, prog=prog)
         pos = nx.multipartite_layout(graph, subset_key="layer", align=align)
+        plt.figure(figsize=figsize)
         nx.draw(graph, pos=pos, with_labels=with_labels, font_size=font_size, font_weight=font_weight,
                 node_color=node_color,  node_size=node_size, arrowsize=arrowsize)
+        #plt.show()
 
         if file != None:
             plt.title(self.name)
