af588aff7958943f4f7446eccb5c1cdb883ab7d2
Rupert Young <rupert@perceptualrobots.com>
2020-12-24 13:20:32 +0000

    update

--- pct/functions.py
+++ pct/functions.py
@@ -71,12 +71,24 @@ class BaseFunction(ABC):
 
             self.links.append(links)
 
-    def draw(self, with_labels=True,  font_size=12, font_weight='bold', node_color='red',
+    def draw(self, with_labels=True,  font_size=12, font_weight='bold', node_color=None,
+             color_mapping={'s':'aqua','c':'limegreen','p':'red', 'x':'silver'},
              node_size=500, arrowsize=25, align='horizontal', file=None):
         graph = self.graph(layer=0, layer_edges=True)
+        if node_color==None:
+            node_color = self.get_colors(graph, color_mapping)
         pos = nx.multipartite_layout(graph, subset_key="layer", align=align)
         nx.draw(graph,  pos=pos, with_labels=with_labels, font_size=font_size, font_weight=font_weight,
                 node_color=node_color,  node_size=node_size, arrowsize=arrowsize)
+
+    def get_colors(self, graph, color_mapping):
+        colors=[]
+        for node in graph:
+            for key in color_mapping.keys():
+                if node.startswith(key):
+                    colors.append(color_mapping[key])
+                    break
+        return colors
 
     def graph(self, layer=None, layer_edges=False):
         graph = nx.DiGraph()
