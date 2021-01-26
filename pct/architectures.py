9ae35dd9858d06ee81e09457a9de922c2c4faaa5
Rupert Young <rupert@perceptualrobots.com>
2021-01-26 19:41:11 +0000

    update

--- pct/architectures.py
+++ pct/architectures.py
@@ -16,7 +16,7 @@ from .functions import IndexedParameter
 from .functions import Constant
 from .environments import PendulumV0
 from .environments import VelocityModel
-from .structure import StructureDefinition
+#from pct.structure import ArchitectureStructure
 from .structure import LevelKey
 
 # Cell
