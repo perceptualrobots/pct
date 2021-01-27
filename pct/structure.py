c9a4d87ab6f80f7fb9d193e8c36d609100e9fd5d
Rupert Young <rupert@perceptualrobots.com>
2021-01-27 19:33:52 +0000

    update

--- pct/structure.py
+++ pct/structure.py
@@ -9,6 +9,7 @@ import numpy as np
 from abc import ABC
 from .functions import FunctionFactory
 from .functions import WeightedSum
+from .functions import ControlUnitFunctions
 from .nodes import PCTNode
 
 # Cell
@@ -252,7 +253,7 @@ class ArchitectureStructure():
     """
 
     def set_output_function(self, node, mode, thislevel, column, input_weights):
-        type = PCTNode.get_function_type(mode, ControlUnitFunction.OUTPUT)
+        type = PCTNode.get_function_type(mode, ControlUnitFunctions.OUTPUT)
         function_type = FunctionFactory.createFunction(type)
         function_type.set_output_function(node, thislevel, column, input_weights)
 
