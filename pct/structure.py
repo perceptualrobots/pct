d157881a0f02e4e49248110e0ae203791941d5c3
Rupert Young <rupert@perceptualrobots.com>
2021-01-27 19:30:29 +0000

    update

--- pct/structure.py
+++ pct/structure.py
@@ -254,7 +254,7 @@ class ArchitectureStructure():
     def set_output_function(self, node, mode, thislevel, column, input_weights):
         type = PCTNode.get_function_type(mode, function)
         function_type = FunctionFactory.createFunction(type)
-        function_type.set_output_function(node, function,  thislevel, column, input_weights)
+        function_type.set_output_function(node, thislevel, column, input_weights)
 
 
     # assume same for all levels and that datatypes are always floats
