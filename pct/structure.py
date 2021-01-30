a4e94abafd7e724fa7f637e18437028bc586efcd
Rupert Young <rupert@perceptualrobots.com>
2021-01-30 10:23:59 +0000

    update

--- pct/structure.py
+++ pct/structure.py
@@ -273,10 +273,9 @@ class ArchitectureStructure():
 
     def set_action_function(self, hpct, env, numColumnsThisLevel,  weights):
         numActions = len(weights)
-        suffix = hpct.get_function(0, 0, 'output').get_suffix()
         for actionIndex in range(numActions):
             action = WeightedSum(weights=weights[actionIndex], name=f'Action{actionIndex+1}')
             for column in range(numColumnsThisLevel):
-                action.add_link(f'OL0C{column}'+suffix)
+                action.add_link(f'OL0C{column}')
             hpct.add_postprocessor(action)
             env.add_link(action)
