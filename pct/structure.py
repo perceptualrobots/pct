d57a209c29801fd1141c826e788ef727d24af91a
Rupert Young <rupert@perceptualrobots.com>
2021-02-17 19:50:45 +0000

    update

--- pct/structure.py
+++ pct/structure.py
@@ -49,6 +49,16 @@ class ArchitectureStructure():
         #function_type.set_node_function(node, function,  thislevel, targetlevel, targetprefix, targetcolumn, num_target_indices, inputs, input_weights, by_column)
 
 
+
+
+    def set_sparse_node_function(node, ControlUnitFunctions.PERCEPTION, level, level-1, 'P',
+                                             column, numColumnsPreviousLevel, None, config[inputsIndex], False)
+        func = node.get_function_from_collection(function)
+        func.set_sparse_node_function(function, thislevel, targetlevel, targetprefix, targetcolumn, num_target_indices, inputs,
+                               input_weights, by_column)
+
+
+
     def get_parameter(self, key):
         return self.config['parameters'][key]
 
