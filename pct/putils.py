4b65ea62d2ed48bb5bf1fd8d075a322a1066eccc
Rupert Young <rupert@perceptualrobots.com>
2021-06-07 20:44:10 +0100

    update

--- pct/putils.py
+++ pct/putils.py
@@ -108,6 +108,7 @@ def get_drive():
 
 # Cell
 def get_gdrive():
+    import socket
     if socket.gethostname() == 'DESKTOP-5O07H5P':
         root_dir='/mnt/c/Users/ruper/Google Drive/'
         if os.name == 'nt' :
