c044aca36521cb85f6f343d84e1b8034567c8f2d
Rupert Young <rupert@perceptualrobots.com>
2021-06-12 12:07:02 +0100

    update

--- pct/functions.py
+++ pct/functions.py
@@ -9,6 +9,7 @@ import numpy as np
 import enum
 from abc import ABC, abstractmethod
 from .putils import sigmoid
+from .putils import smooth
 from .putils import UniqueNamer
 from .putils import FunctionsList
 
