a6642f1d8948eefc6607a0b88ba2dee8cfde5f3f
Rupert Young <rupert@perceptualrobots.com>
2021-07-22 19:06:39 +0100

    update

--- pct/hierarchy.py
+++ pct/hierarchy.py
@@ -512,7 +512,7 @@ class PCTHierarchy():
 
         pre = {}
         for i in range(len(self.preCollection)):
-            pre[f'pre{i}']=self.preCollection[0].get_config()
+            pre[f'pre{i}']=self.preCollection[i].get_config()
         config['pre']=pre
 
 
@@ -533,7 +533,7 @@ class PCTHierarchy():
 
         post = {}
         for i in range(len(self.postCollection)):
-            post[f'post{i}']=self.postCollection[0].get_config()
+            post[f'post{i}']=self.postCollection[i].get_config()
         config['post']=post
         return config
 
