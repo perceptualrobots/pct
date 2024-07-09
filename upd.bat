1ec640401f9fbba09b261838450615083ef06a96
perceptualrobots <rupert@perceptualrobots.com>
2024-07-09 19:04:37 +0100

    before updating ARCDataProcessor, before restructuring ARCEnv

--- upd.bat
+++ upd.bat
@@ -1,8 +1,8 @@
 @echo off
 echo %time%
 git add -A
--- git commit -m "before restructuring ARCEnv"
-git commit -m "up"
+git commit -m "before updating ARCDataProcessor, before restructuring ARCEnv"
+rem git commit -m "up"
 git pull
 git push
 echo %time%
