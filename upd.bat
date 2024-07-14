8872287ee52812652172ef4d7fcef6044d7e06f3
perceptualrobots <rupert@perceptualrobots.com>
2024-07-14 20:59:07 +0100

    u

--- upd.bat
+++ upd.bat
@@ -3,8 +3,8 @@ echo %time%
 git add -A
 rem git commit -m "after restructuring ARCEnv"
 git commit -m "up"
-git commit -m "after modifying ARCDataProcessor"
-rem git commit -m "up"
+rem git commit -m "after modifying ARCDataProcessor"
+git commit -m "up"
 git pull
 git push
 echo %time%
