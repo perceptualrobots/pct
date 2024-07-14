6dca01e457b6556e636a916e7820e1ee642c56dd
perceptualrobots <rupert@perceptualrobots.com>
2024-07-14 20:56:47 +0100

    up

--- upd.bat
+++ upd.bat
@@ -1,8 +1,8 @@
 @echo off
 echo %time%
 git add -A
-git commit -m "after restructuring ARCEnv"
-rem git commit -m "up"
+rem git commit -m "after restructuring ARCEnv"
+git commit -m "up"
 git pull
 git push
 echo %time%
