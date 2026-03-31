743a32b13c4f86d84f8ca899da16a2e6a52ff029
perceptualrobots <rupert@perceptualrobots.com>
2026-03-31 15:10:38 +0100

    Update example command for LunarLander to include early termination option

--- pct/applications/pct_example.py
+++ pct/applications/pct_example.py
@@ -28,14 +28,10 @@ Examples:
   
   # Run with rendering and verbose output
   python pct/applications/pct_example.py 'testfiles/MountainCar/MountainCar-cdf7cc.properties' --render --verbose
-  python pct/applications/pct_example.py 'testfiles/LunarLander/LunarLander-4905d2.properties' --render --verbose
+  python pct/applications/pct_example.py 'testfiles/LunarLander/LunarLander-4905d2.properties' --render --early-termination --verbose
   python pct/applications/pct_example.py 'testfiles/CartPole/CartPole-6fb461.properties' --render --verbose
 
   python pct/applications/pct_example.py 'testfiles/CartPole/CartPole-6150fd-reward.properties' --render 
-
-
-
-
 
   # Run with image output
   python pct/applications/pct_example.py 'testfiles/MountainCar/MountainCar-cdf7cc.properties' --image --image-size 16 10
